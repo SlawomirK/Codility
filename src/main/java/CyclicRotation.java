eaa45d1c3d870c721114799dc5724c19570766be
Slawek <Kobylinski.s@gmail.com>
2021-11-30 15:20:25 +0100

    Initial commit

--- src/main/java/CyclicRotation.java
+++ src/main/java/CyclicRotation.java
@@ -2,13 +2,31 @@
  * Copyright (c) 2021. Sławomir Kobyliński
  */
 
+import java.util.Arrays;
+
 public class CyclicRotation {
 
-    public CyclicRotation(){
-
+    public int[] solution(int[] A, int k){
+        if(k==0 || k==A.length || checkDif(A))
+        return A;
+        else return goCyclic(A,k);
     }
-    public int[] solution(int[] A, int K){
-        int[] resultArray
+
+    private boolean checkDif(int[] a) {
+       int difference= Math.toIntExact(Arrays.stream(a).distinct().count());
+       return difference == 1;
+    }
+
+    private int[] goCyclic(int[] a, int k) {
+        int leng=a.length;
+
+        int[] resultArray = new int[leng];
+        for(int i=0;i<leng;i++){
+            if(i+k>leng-1){
+                resultArray[i-k+1]=a[i];
+            }else
+            resultArray[i+k]=a[i];
+        }
         return resultArray;
     }
 }
--- src/test/java/CyclicRotation_Test.java
+++ src/test/java/CyclicRotation_Test.java
@@ -9,44 +9,40 @@ import static org.junit.Assert.assertArrayEquals;
 
 public class CyclicRotation_Test {
 
-    CyclicRotation cyclicRotation;
-    @BeforeEach
-    void beforeEach(){
-        cyclicRotation=new CyclicRotation();
-    }
+    CyclicRotation cyclicRotation=new CyclicRotation();
+
     @Test
-    void shouldReturnTrue1(){
+    public void shouldReturnTrue1() {
         //given
-        int[] A=new int[]{3, 8, 9, 7, 6};
-        int k=3;
+        int[] A = new int[]{3, 8, 9, 7, 6};
+        int k = 3;
         //when
-        int[] result=cyclicRotation.solution(A,k);
+        int[] result = cyclicRotation.solution(A, k);
         //then
-        assertArrayEquals(result,new int[]{9, 7, 6, 3, 8});
+        assertArrayEquals(new int[]{9, 7, 6, 3, 8},result );
     }
 
     @Test
-    void shouldReturnTrue2(){
+    public void shouldReturnTrue2() {
         //given
-        int[] A=new int[]{0,0,0};
-        int k=1;
+        int[] A = new int[]{0, 0, 0};
+        int k = 1;
         //when
-        int[] result=cyclicRotation.solution(A,k);
+        int[] result = cyclicRotation.solution(A, k);
         //then
-        assertArrayEquals(result,new int[]{0,0,0});
+        assertArrayEquals(result, new int[]{0, 0, 0});
     }
 
     @Test
-    void shouldReturnTrueOK(){
+    public void shouldReturnTrueOK() {
         //given
-        int[] A=new int[]{1,2,3,4};
-        int k=4;
+        int[] A = new int[]{1, 2, 3, 4};
+        int k = 4;
         //when
-        int[] result=cyclicRotation.solution(A,k);
+        int[] result = cyclicRotation.solution(A, k);
         //then
-        assertArrayEquals(result,A);
+        assertArrayEquals(result, A);
     }
-
 
 
 }
